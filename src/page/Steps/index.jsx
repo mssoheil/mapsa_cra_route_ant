d05b1a9823147598d9a8d0dbf6684096c15ee8c9
Soheil Pasbakhsh <mssoheil88@yahoo.com>
2023-01-16 20:56:12 +0330

    feat: add steps

--- src/page/Steps/index.jsx
+++ src/page/Steps/index.jsx
@@ -1,5 +1,45 @@
+// UI frameworks
+import { Steps as AntdSteps } from "antd";
+import { HeartFilled } from "@ant-design/icons";
+
+const items = [
+  {
+    title: "Step1",
+    subTitle: "sub1",
+    description: "desc1",
+    // icon: <HeartFilled />,
+  },
+  {
+    title: "Step1",
+    subTitle: "sub1",
+    description: "desc2",
+  },
+  {
+    title: "Step1",
+    subTitle: "sub1",
+    description: "desc3",
+  },
+];
+
+const dot = (dot) => {
+  return <div>{dot}</div>;
+};
+
 const Steps = () => {
-  return <div>steps</div>;
+  return (
+    <div>
+      <AntdSteps
+        current={1}
+        // percent={67}
+        // type="inline"
+        items={items}
+        labelPlacement="vertical"
+        size="small"
+        // progressDot={dot}
+        direction="vertical"
+      />
+    </div>
+  );
 };
 
 export default Steps;
