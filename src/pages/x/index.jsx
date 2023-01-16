91db60b375e3ec52fb862eedc7151b64a45776f9
Soheil Pasbakhsh <mssoheil88@yahoo.com>
2023-01-16 17:38:32 +0330

    ci: init

--- src/pages/x/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const X = () => {
-  return <div>X</div>;
-};
-
-export default X;
--- src/pages/y/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Y = () => {
-  return <div>Y</div>;
-};
-
-export default Y;
