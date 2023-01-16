997ff802618d2b586977c154a301cd4d3351a9bd
Soheil Pasbakhsh <mssoheil88@yahoo.com>
2023-01-16 19:29:13 +0330

    feat: add drop down

--- src/page/DropDown/index.jsx
+++ src/page/DropDown/index.jsx
@@ -1,5 +1,31 @@
+import { Button, Dropdown } from "antd";
+
+const items = [
+  {
+    key: "a",
+    label: <span>hello</span>,
+    danger: true,
+    disabled: true,
+  },
+  {
+    key: "b",
+    label: "item b",
+    children: [{ key: "b", label: "item b" }],
+  },
+  {
+    key: "c",
+    label: "item c",
+  },
+];
+
 const DropDown = () => {
-  return <div>DropDown</div>;
+  return (
+    <div>
+      <Dropdown menu={{ items }} placement="bottomRight" arrow>
+        <Button>hover</Button>
+      </Dropdown>
+    </div>
+  );
 };
 
 export default DropDown;
