db069ff67ac77e86d623f1040a2d866069d8923b
Soheil Pasbakhsh <mssoheil88@yahoo.com>
2023-01-16 19:10:05 +0330

    feat(App): handle custom collapse

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 // UI frameworks
-import { Layout, Menu } from "antd";
+import { Button, Layout, Menu } from "antd";
 import { MenuUnfoldOutlined } from "@ant-design/icons";
 import { Route, Routes, useNavigate } from "react-router";
 // Pages
@@ -27,6 +27,8 @@ const menuItems = [
 const App = () => {
   const navigate = useNavigate();
 
+  // const [collapsed, setCollapsed] = React.useState(false);
+
   function handleClickItem(event) {
     const { key } = event;
     switch (key) {
@@ -44,42 +46,54 @@ const App = () => {
     }
   }
 
+  // custom collapse
+  // function handleToggleCollapse() {
+  //   setCollapsed((collapsed) => !collapsed);
+  // }
+
   return (
-    <div>
-      {/* <Layout> */}
-      {/* header/content/footer */}
-      {/* <Layout.Header style={{ background: "green" }}>header</Layout.Header>
+    <Layout>
+      {/* <Layout style={{ height: "100vh" }}>
+        <Layout.Header style={{ background: "green" }}>header</Layout.Header>
         <Layout.Content style={{ background: "blue" }}>content</Layout.Content>
-        <Layout.Footer style={{ background: "red" }}>footer</Layout.Footer> */}
-      {/* header/sider-content/footer */}
-      {/* <Layout.Sider style={{ background: "blue" }}>sider</Layout.Sider>
+        <Layout.Footer style={{ background: "red" }}>footer</Layout.Footer>
+
+        <Layout.Sider
+          collapsible
+          collapsed={collapsed}
+          style={{ background: "blue" }}
+          trigger={<span>close</span>}
+        >
+          sider
+        </Layout.Sider>
         <Layout>
-          <Layout.Header style={{ background: "green" }}>header</Layout.Header>
+          <Layout.Header style={{ background: "green" }}>
+            header
+            <Button onClick={handleToggleCollapse}>Toggle Collapse</Button>
+          </Layout.Header>
           <Layout.Content style={{ background: "red" }}>content</Layout.Content>
           <Layout.Footer style={{ background: "orange" }}>footer</Layout.Footer>
-        </Layout> */}
-      {/* </Layout> */}
+        </Layout>
+      </Layout> */}
 
-      <Layout>
-        <Layout.Header>
-          <Menu
-            items={menuItems}
-            mode="horizontal"
-            theme="dark"
-            defaultSelectedKeys={[]}
-            onClick={handleClickItem}
-          />
-        </Layout.Header>
-        <Layout.Content>
-          <Routes>
-            <Route path="/drop-down" element={<DropDown />} />
-            <Route path="/paginations" element={<Paginations />} />
-            <Route path="/steps" element={<Steps />} />
-          </Routes>
-        </Layout.Content>
-        <Layout.Footer>footer</Layout.Footer>
-      </Layout>
-    </div>
+      <Layout.Header>
+        <Menu
+          items={menuItems}
+          mode="horizontal"
+          theme="dark"
+          defaultSelectedKeys={[]}
+          onClick={handleClickItem}
+        />
+      </Layout.Header>
+      <Layout.Content>
+        <Routes>
+          <Route path="/drop-down" element={<DropDown />} />
+          <Route path="/paginations" element={<Paginations />} />
+          <Route path="/steps" element={<Steps />} />
+        </Routes>
+      </Layout.Content>
+      <Layout.Footer>footer</Layout.Footer>
+    </Layout>
   );
 };
 
