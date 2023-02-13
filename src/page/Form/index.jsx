75a9e86e3e98d0d4f73d036436f75e1c208aa846
mssoheil88 <mssoheil88@yahoo.com>
2023-02-13 20:19:01 +0330

    feat: add custom validator

--- src/page/Form/index.jsx
+++ src/page/Form/index.jsx
@@ -16,21 +16,45 @@ const Form = () => {
     }
 
     function handleResetForm() {
-        console.log("HERE")
         form.resetFields();
     }
 
+    function handleChangeEmail(event) {
+        const {value} = event.target;
+        if(isNaN(value)) {
+            form.setFieldValue("email", value.replace(/\w/g, ""))
+        } else {
+            form.setFieldValue("email", value)
+        }
+        
+    }
+
+    async function emailValidator(_, value) {
+        if(value.length <= 5) {
+            return Promise.reject("the email should at least have 5 characters")
+        }
+        return Promise.resolve();
+       
+
+    } 
+
   return (
     <React.Fragment>
-        <AntdForm name="login-form" form={form} onFinish={handleFinish} onFinishFailed={handleError}>
-            <AntdForm.Item label="email" rules={[
-                {
-                    required: true,
-                    message: "Please provide email"
-                }
-            ]} name="email">
+        <AntdForm name="login-form" form={form} size="large"
+        layout="inline"
+        onFinish={handleFinish} onFinishFailed={handleError}
+        validateTrigger="onChange" colon={false} requiredMark={false}
+        // disabled
+        >
+            <AntdForm.Item label="email"  name="email" onChange={handleChangeEmail}>
                 <Input />
             </AntdForm.Item>
+            <AntdForm.Item label="validated" name="validated" rules={[
+                {validator: emailValidator}
+            ]} >
+                <Input />
+            </AntdForm.Item>
+            
             <Button htmlType="submit">submit</Button>
             <Button onClick={handleResetForm}>reset</Button>
         </AntdForm>
