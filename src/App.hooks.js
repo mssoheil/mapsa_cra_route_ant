d1344bbc770f565f4483f79d92fbd825f9714b96
mssoheil88 <mssoheil88@yahoo.com>
2023-02-13 19:04:22 +0330

    refactor(App): separate logic to hook

---
+++ src/App.hooks.js
@@ -0,0 +1,108 @@
+import React from "react";
+// UI frameworks
+import { MenuUnfoldOutlined } from "@ant-design/icons";
+// Hooks
+import { useNavigate, useLocation } from "react-router";
+
+const menuItems = [
+  {
+    key: "drop-down",
+    label: "Drop down",
+    theme: "light",
+    icon: <MenuUnfoldOutlined />,
+  },
+  {
+    key: "steps",
+    label: "Steps",
+  },
+  {
+    key: "paginations",
+    label: "Pagination",
+  },
+  {
+    key: "divider",
+    label: "divider",
+  },
+  {
+    key: "listWithPagination",
+    label: "List with pagination",
+  },
+  {
+    key: "autoComplete",
+    label: "Auto Complete",
+  },
+  {
+    key: "checkbox",
+    label: "Checkbox",
+  },
+  {
+    key: "codeSplitting",
+    label: "Code Splitting",
+  },
+  {
+    key: "users",
+    label: "Users",
+  },
+  {
+    key: "animations",
+    label: "Animations",
+  },
+  {
+    key: "inputs",
+    label: "Inputs",
+  },
+];
+
+export function useApp() {
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+
+  const [theme, setTheme] = React.useState("light");
+
+  function handleClickItem(event) {
+    const { key } = event;
+    switch (key) {
+      case "drop-down":
+        navigate("/drop-down");
+        break;
+      case "paginations":
+        navigate("/paginations");
+        break;
+      case "steps":
+        navigate("/steps");
+        break;
+      case "divider":
+        navigate("/divider");
+        break;
+      case "listWithPagination":
+        navigate("/list-with-pagination");
+        break;
+      case "autoComplete":
+        navigate("/auto-complete");
+        break;
+      case "checkbox":
+        navigate("/checkbox");
+        break;
+      case "codeSplitting":
+        navigate("/code-splitting");
+        break;
+      case "users":
+        navigate("/users");
+        break;
+      case "animations":
+        navigate("/animations");
+        break;
+      case "inputs":
+        navigate("/inputs");
+        break;
+      default:
+        break;
+    }
+  }
+
+  function toggleTheme() {
+    setTheme((theme) => (theme === "light" ? "dark" : "light"));
+  }
+
+  return { theme, toggleTheme, menuItems, handleClickItem, pathname };
+}
--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,8 @@ import AutoComplete from "./page/AutoComplete";
 import CheckBox from "./page/CheckBox";
 import Users from "./page/Users";
 import Animations from "./page/Animations";
+// Hooks
+import { useApp } from "./App.hooks";
 // Styles
 import "./app-style.css";
 // Store
@@ -35,100 +37,9 @@ const CodeSplitting = React.lazy(async () => {
 })
 
 
-const menuItems = [
-  {
-    key: "drop-down",
-    label: "Drop down",
-    theme: "light",
-    icon: <MenuUnfoldOutlined />,
-  },
-  {
-    key: "steps",
-    label: "Steps",
-  },
-  {
-    key: "paginations",
-    label: "Pagination",
-  },
-  {
-    key: "divider",
-    label: "divider",
-  },
-  {
-    key: "listWithPagination",
-    label: "List with pagination",
-  },
-  {
-    key: "autoComplete",
-    label: "Auto Complete",
-  },
-  {
-    key: "checkbox",
-    label: "Checkbox",
-  },
-  {
-    key: "codeSplitting",
-    label: "Code Splitting",
-  },
-  {
-    key: "users",
-    label: "Users",
-  },
-  {
-    key: "animations",
-    label: "Animations",
-  },
-];
-
-
 const App = () => {
-  const navigate = useNavigate();
-  const { pathname } = useLocation();
-
-  const [theme, setTheme] = React.useState("light");
-
-
-  function handleClickItem(event) {
-    const { key } = event;
-    switch (key) {
-      case "drop-down":
-        navigate("/drop-down");
-        break;
-      case "paginations":
-        navigate("/paginations");
-        break;
-      case "steps":
-        navigate("/steps");
-        break;
-      case "divider":
-        navigate("/divider");
-        break;
-      case "listWithPagination":
-        navigate("/list-with-pagination");
-        break;
-      case "autoComplete":
-        navigate("/auto-complete");
-        break;
-      case "checkbox":
-        navigate("/checkbox");
-        break;
-      case "codeSplitting":
-        navigate("/code-splitting");
-        break;
-      case "users":
-        navigate("/users");
-        break;
-      case "animations":
-        navigate("/animations");
-        break;
-      default:
-        break;
-    }
-  }
-
-  function toggleTheme() {
-    setTheme(theme => theme === "light" ? "dark" : "light")
-  }
+  
+  const { theme, toggleTheme, menuItems, handleClickItem, pathname } = useApp()  
 
 
   return (
